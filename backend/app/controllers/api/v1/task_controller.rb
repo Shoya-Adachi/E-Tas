fd444c5a851986224b9fc473447400b61c9c5ce4
OtaShoya <shoya.toawork@gmail.com>
2025-05-01 22:03:45 +0900

    作成＆削除api作成

--- backend/app/controllers/api/v1/task_controller.rb
+++ backend/app/controllers/api/v1/task_controller.rb
@@ -5,4 +5,29 @@ class Api::V1::TaskController < ApplicationController
     render json: tasks
   end
 
+  def create
+    tasks = Task.build(
+      name: params[:name],      
+      period: params[:period],  
+      status: params[:status], 
+    )
+
+    if tasks.save
+      render json: { status: 'success', data: tasks }, status: :created
+    else
+      render json: { status: 'error', errors: tasks.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    tasks = Task.find(params[:id])
+
+    if tasks
+      tasks.destroy
+      head :no_content
+    else
+      render json: { error: 'task not found' }, status: :not_found
+    end
+  end
+
 end
